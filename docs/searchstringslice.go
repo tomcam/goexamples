f70a1f6432bdd78013254b1f2ae90d2eb208ad65
Tom Campbell <tomcampbell@gmail.com>
2019-11-18 23:22:05 -0800

    Create searchstringslice.go

---
+++ docs/searchstringslice.go
@@ -0,0 +1,41 @@
+// Shows how to sort a string slice, then search it.
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+var excludedDirs = []string{".git", "pub", ".backup"}
+
+// sortStringSlice does just that if you pass it the address of a string slice.
+func sortStringSlice(s *[]string) {
+	sort.Slice(*s, func(i, j int) bool {
+		return (*s)[i] <= (*s)[j]
+	})
+}
+
+// inSlice() returns true if the search term is found in a sorted slice.
+func inSlice(search string, s []string) bool {
+	pos := sort.Search(len(s), func(i int) bool {
+		return string(s[i]) >= search
+	})
+
+	return s[pos] == search
+}
+func main() {
+	searchFor := ".git"
+	sortStringSlice(&excludedDirs)
+	if inSlice(searchFor, excludedDirs) {
+		fmt.Printf("Found %s\n", searchFor)
+	} else {
+		fmt.Printf("Couldn't find %s\n", searchFor)
+	}
+	searchFor = "node_modules"
+	if inSlice(searchFor, excludedDirs) {
+		fmt.Printf("Found %s\n", searchFor)
+	} else {
+		fmt.Printf("Couldn't find %s\n", searchFor)
+	}
+
+}
