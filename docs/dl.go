35fd79de5c0330a71a4bcce1f4528241e2877f71
Tom Campbell <tomcampbell@gmail.com>
2022-09-13 01:01:17 -0700

    Update dl.go

--- docs/dl.go
+++ docs/dl.go
@@ -1,5 +1,7 @@
 // dl displays a list of directories. Starts at current dir unless
 // another is given on the command line. Counts theme.
+// Naively written; doesn't do a good job when you don't have 
+// the right permissions.
 // Usage:
 //   dl [directory]
 
@@ -14,6 +16,7 @@ import (
 func visit(files *[]string, count *int) filepath.WalkFunc {
 	return func(path string, info os.FileInfo, err error) error {
 		if err != nil {
+      fmt.Printf("Error at %v: %v", path,  err.Error())
 			return err
 		}
 		isDir := info.IsDir()
@@ -36,6 +39,7 @@ func getDirTree(path string, dirs *int) (tree []string, err error) {
 	var files []string
 	err = filepath.Walk(path, visit(&files, dirs))
 	if err != nil {
+    fmt.Printf("Error at %v: %v", path,  err.Error())
 		return []string{}, err
 	}
 	return files, nil
@@ -52,6 +56,10 @@ func main() {
 	if err != nil {
 		fmt.Printf("Error: %v", err.Error())
 	} else {
-		fmt.Printf("%v directories\n", dirCount)
+		if dirCount != 1 {
+      fmt.Printf("%v directories\n", dirCount)
+    } else {
+      fmt.Printf("%v directory\n", dirCount)
+    }
 	}
 }
