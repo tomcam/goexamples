3877bfb8911a8f4e2a4b7ab41e14f573d8ed6166
Tom Campbell <tomcampbell@gmail.com>
2023-01-25 13:26:18 -0800

    Use the more robust embed all

--- docs/embedlistdir.go
+++ docs/embedlistdir.go
@@ -1,35 +1,52 @@
-// embedlistdir() shows how to embed a subdirectory of the package
-// named themes into the executable at compile time, then get
+// Shows how to embed a subdirectory of the package
+// named .config into the executable at compile time, then get
 // access to its contents at runtime.
 
 package main
-
 import (
 	"embed"
-	"fmt"
 	"io/fs"
+	"fmt"
+ 	"io/ioutil"
+ 	"os"
 )
 
 
 // The following embeds all files and subdirectories
 // from the themes subdirectory of this package into
-// the executable.
-
-//go:embed themes/*
-var themeFiles embed.FS
+// the executable. Can change directory name from .config to anything else.
+//go:embed all:.config
+var configFiles embed.FS
 
 // embedListDir() displays the filenames in the embedded 
-// directory named theme.
+// directory passed to the files parameter.
 func embedListDir(files embed.FS) error {
 	fs.WalkDir(files, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
-		fmt.Println(path)
+		print(path)
 		return nil
 	})
 	return nil
 }
+
+// Open file name and send its contents to stdout.
+func print(filename string) {
+  f, err := os.Open(filename); 
+  if err != nil {
+    fmt.Println("Error opening %s: %v", filename, err.Error())
+    os.Exit(1)
+  }
+  bytes, err := ioutil.ReadAll(f); 
+  if err != nil {
+    fmt.Println("Error reading %s: %v", filename, err.Error())
+  } else {
+    fmt.Println(string(bytes))
+    os.Exit(1)
+  }
+}
+
 func main() {
-	embedListDir(themeFiles)
+	embedListDir(configFiles)
 }
